62641af23567999e2643d0ffd1a95884202c416e
Olivier Sohn <olivier.sohn@gmail.com>
2024-07-29 14:55:05 +0200

    Test number of SQL queries

--- src/Tests.cpp
+++ src/Tests.cpp
@@ -7,26 +7,81 @@
 namespace test
 {
 
+struct GraphWithStats
+{
+  GraphWithStats();
+  
+  GraphDB& getDB() { return *m_graph; }
+
+  size_t totalSQLQueriesCount() const { return m_countSQLQueries; }
+private:
+  std::unique_ptr<GraphDB> m_graph;
+  size_t m_countSQLQueries{};
+
+  bool m_printSQLRequests{true};
+};
+
+GraphWithStats::GraphWithStats()
+{
+  auto onSQLQuery = [&](const std::string& req)
+  {
+    ++m_countSQLQueries;
+    if(m_printSQLRequests)
+    {
+      bool first = true;
+      for(const auto & part1 : splitOn("UNION ALL ", req))
+        for(const auto & part : splitOn("INNER JOIN ", part1))
+        {
+          std::cout << LogIndent{};
+          if(first)
+          {
+            first = false;
+            std::cout << "[SQL] ";
+          }
+          else
+            std::cout << "      ";
+          std::cout << part << std::endl;
+        }
+    }
+  };
+  auto onDBDiagnosticContent = [&](int argc, char **argv, char **column)
+  {
+    if(m_printSQLRequests)
+    {
+      auto _ = LogIndentScope{};
+      std::cout << LogIndent{};
+      for (int i=0; i < argc; i++)
+        printf("%s,\t", argv[i]);
+      printf("\n");
+    }
+    return 0;
+  };
+
+  m_graph = std::make_unique<GraphDB>(onSQLQuery, onDBDiagnosticContent);
+}
+
+
 struct QueryResultsHandler
 {
-  QueryResultsHandler(GraphDB& db)
+  QueryResultsHandler(GraphWithStats& db)
   : m_db(db)
   {}
   
   void run(const std::string &cypherQuery)
   {
-    openCypher::runCypher(cypherQuery, m_db, *this);
+    openCypher::runCypher(cypherQuery, m_db.getDB(), *this);
   }
-
+  
   bool printCypherAST() const { return false; }
-
+  
   bool m_printCypherQueryText{ false };
   bool m_printCypherRows{ false };
-
+  
   void onCypherQueryStarts(std::string const & cypherQuery)
   {
     m_rows.clear();
-
+    m_countSQLRequestsAtBeginning = m_db.totalSQLQueriesCount();
+    
     if(m_printCypherQueryText)
     {
       std::cout << std::endl;
@@ -63,6 +118,8 @@ struct QueryResultsHandler
   
   size_t countRows() const { return m_rows.size(); }
   const auto& rows() const { return m_rows; }
+  
+  size_t countSQLQueries() const { return m_db.totalSQLQueriesCount() - m_countSQLRequestsAtBeginning; }
 
 private:
   std::unique_ptr<LogIndentScope> m_logIndentScope;
@@ -70,63 +127,23 @@ private:
   std::vector<std::string> m_variablesNames;
   GraphDB::VecColumnNames m_columnNames;
   
-  GraphDB& m_db;
+  GraphWithStats& m_db;
   std::vector<std::vector<std::optional<std::string>>> m_rows;
+  size_t m_countSQLRequestsAtBeginning{};
 };
-
-
-std::unique_ptr<GraphDB> createDB()
-{
-  const bool printSQLRequests{true};
-  auto onSQLQuery = [=](const std::string& req)
-  {
-    if(printSQLRequests)
-    {
-      bool first = true;
-      for(const auto & part1 : splitOn("UNION ALL ", req))
-        for(const auto & part : splitOn("INNER JOIN ", part1))
-        {
-          std::cout << LogIndent{};
-          if(first)
-          {
-            first = false;
-            std::cout << "[SQL] ";
-          }
-          else
-            std::cout << "      ";
-          std::cout << part << std::endl;
-        }
-    }
-  };
-  auto onDBDiagnosticContent = [=](int argc, char **argv, char **column)
-  {
-    if(printSQLRequests)
-    {
-      auto _ = LogIndentScope{};
-      std::cout << LogIndent{};
-      for (int i=0; i < argc; i++)
-        printf("%s,\t", argv[i]);
-      printf("\n");
-    }
-    return 0;
-  };
-
-  return std::make_unique<GraphDB>(onSQLQuery, onDBDiagnosticContent);
-}
-
 
 TEST(Test, EmptyDB)
 {
   LogIndentScope _{};
   
-  auto db = createDB();
+  auto dbWrapper = std::make_unique<GraphWithStats>();
   
-  QueryResultsHandler handler(*db);
+  QueryResultsHandler handler(*dbWrapper);
   
   handler.run("MATCH (n) RETURN id(n)");
   
   EXPECT_EQ(0, handler.countRows());
-  
+
   handler.run("MATCH (n) RETURN n.propertyDoesNotExist");
   
   EXPECT_EQ(0, handler.countRows());
@@ -144,28 +161,32 @@ TEST(Test, SingleEntity)
 {
   LogIndentScope _{};
   
-  auto db = createDB();
-  
-  db->addType("Entity", true, {});
+  auto dbWrapper = std::make_unique<GraphWithStats>();
 
-  const std::string entityID = db->addNode("Entity", {});
+  auto & db = dbWrapper->getDB();
 
-  QueryResultsHandler handler(*db);
+  db.addType("Entity", true, {});
+
+  const std::string entityID = db.addNode("Entity", {});
+
+  QueryResultsHandler handler(*dbWrapper);
   
   handler.run("MATCH (n) RETURN id(n)");
   
   EXPECT_EQ(1, handler.countRows());
   EXPECT_EQ(entityID, handler.rows()[0][0]);
-  
+  EXPECT_EQ(1, handler.countSQLQueries());
+
   handler.run("MATCH (n) RETURN n.propertyDoesNotExist");
   
   EXPECT_EQ(1, handler.countRows());
   EXPECT_EQ(std::nullopt, handler.rows()[0][0]);
+  EXPECT_EQ(1, handler.countSQLQueries());
 
   handler.run("MATCH ()-[r]->() RETURN id(r)");
   
   EXPECT_EQ(0, handler.countRows());
-  
+
   handler.run("MATCH ()-[r]->() RETURN r.propertyDoesNotExist");
   
   EXPECT_EQ(0, handler.countRows());
@@ -175,45 +196,52 @@ TEST(Test, SingleRecursiveRelationship)
 {
   LogIndentScope _{};
   
-  auto db = createDB();
+  auto dbWrapper = std::make_unique<GraphWithStats>();
+
+  auto & db = dbWrapper->getDB();
+
+  db.addType("Entity", true, {});
+  db.addType("Relationship", false, {});
   
-  db->addType("Entity", true, {});
-  db->addType("Relationship", false, {});
+  const std::string entityID = db.addNode("Entity", {});
+  const std::string relationshipID = db.addRelationship("Relationship", entityID, entityID, {});
   
-  const std::string entityID = db->addNode("Entity", {});
-  const std::string relationshipID = db->addRelationship("Relationship", entityID, entityID, {});
-  
-  QueryResultsHandler handler(*db);
+  QueryResultsHandler handler(*dbWrapper);
   
   handler.run("MATCH (n) RETURN id(n)");
   
   EXPECT_EQ(1, handler.countRows());
   EXPECT_EQ(entityID, handler.rows()[0][0]);
-  
+  EXPECT_EQ(1, handler.countSQLQueries());
+
   handler.run("MATCH ()-[r]->() RETURN id(r)");
   
   EXPECT_EQ(1, handler.countRows());
   EXPECT_EQ(relationshipID, handler.rows()[0][0]);
-  
+  EXPECT_EQ(1, handler.countSQLQueries());
+
   handler.run("MATCH ()-[r]->() RETURN r.propertyDoesNotExist");
   
   EXPECT_EQ(1, handler.countRows());
   EXPECT_EQ(std::nullopt, handler.rows()[0][0]);
-  
+  EXPECT_EQ(1, handler.countSQLQueries());
+
   handler.run("MATCH (a)-[r]->(b) RETURN id(a), id(r), id(b)");
   
   EXPECT_EQ(1, handler.countRows());
   EXPECT_EQ(relationshipID, handler.rows()[0][1]);
   EXPECT_EQ(entityID, handler.rows()[0][0]);
   EXPECT_EQ(entityID, handler.rows()[0][2]);
-  
+  EXPECT_EQ(1, handler.countSQLQueries());
+
   handler.run("MATCH (a)<-[r]-(b) RETURN id(a), id(r), id(b)");
   
   EXPECT_EQ(1, handler.countRows());
   EXPECT_EQ(relationshipID, handler.rows()[0][1]);
   EXPECT_EQ(entityID, handler.rows()[0][0]);
   EXPECT_EQ(entityID, handler.rows()[0][2]);
-  
+  EXPECT_EQ(1, handler.countSQLQueries());
+
   handler.run("MATCH (a)-[R]-(B) RETURN id(a), id(R), id(B)");
   
   EXPECT_EQ(2, handler.countRows());
@@ -223,19 +251,22 @@ TEST(Test, SingleRecursiveRelationship)
   EXPECT_EQ(relationshipID, handler.rows()[1][1]);
   EXPECT_EQ(entityID, handler.rows()[1][0]);
   EXPECT_EQ(entityID, handler.rows()[1][2]);
-  
+  EXPECT_EQ(2, handler.countSQLQueries());  // TODO optimize
+
   handler.run("MATCH (a)-[r]->(a) RETURN id(a), id(r)");
   
   EXPECT_EQ(1, handler.countRows());
   EXPECT_EQ(relationshipID, handler.rows()[0][1]);
   EXPECT_EQ(entityID, handler.rows()[0][0]);
-  
+  EXPECT_EQ(1, handler.countSQLQueries());
+
   handler.run("MATCH (a)<-[r]-(a) RETURN id(a), id(r)");
   
   EXPECT_EQ(1, handler.countRows());
   EXPECT_EQ(relationshipID, handler.rows()[0][1]);
   EXPECT_EQ(entityID, handler.rows()[0][0]);
-  
+  EXPECT_EQ(1, handler.countSQLQueries());
+
   handler.run("MATCH (a)-[R]-(a) RETURN id(a), id(R)");
   
   EXPECT_EQ(2, handler.countRows());
@@ -243,12 +274,14 @@ TEST(Test, SingleRecursiveRelationship)
   EXPECT_EQ(entityID, handler.rows()[0][0]);
   EXPECT_EQ(relationshipID, handler.rows()[1][1]);
   EXPECT_EQ(entityID, handler.rows()[1][0]);
-  
+  EXPECT_EQ(2, handler.countSQLQueries()); // TODO optimize
+
   // id(a) <> id(b) is a constraint enforced while scanning the system relationships table.
   handler.run("MATCH (a)-[r]->(b) WHERE id(a) <> id(b) RETURN id(a), id(r), id(b)");
   
   EXPECT_EQ(0, handler.countRows());
-  
+  EXPECT_EQ(1, handler.countSQLQueries());
+
   // id(a) = id(b) is a constraint enforced while scanning the system relationships table.
   handler.run("MATCH (a)-[r]->(b) WHERE id(a) = id(b) RETURN id(a), id(r), id(b)");
 
@@ -256,7 +289,7 @@ TEST(Test, SingleRecursiveRelationship)
   EXPECT_EQ(relationshipID, handler.rows()[0][1]);
   EXPECT_EQ(entityID, handler.rows()[0][0]);
   EXPECT_EQ(entityID, handler.rows()[0][2]);
-
+  EXPECT_EQ(1, handler.countSQLQueries());
 }
 
 
@@ -264,16 +297,17 @@ TEST(Test, SingleNonRecursiveRelationship)
 {
   LogIndentScope _{};
   
-  auto db = createDB();
+  auto dbWrapper = std::make_unique<GraphWithStats>();
+
+  auto & db = dbWrapper->getDB();
+  db.addType("Entity", true, {});
+  db.addType("Relationship", false, {});
   
-  db->addType("Entity", true, {});
-  db->addType("Relationship", false, {});
+  const std::string entityIDSource = db.addNode("Entity", {});
+  const std::string entityIDDestination = db.addNode("Entity", {});
+  const std::string relationshipID = db.addRelationship("Relationship", entityIDSource, entityIDDestination, {});
   
-  const std::string entityIDSource = db->addNode("Entity", {});
-  const std::string entityIDDestination = db->addNode("Entity", {});
-  const std::string relationshipID = db->addRelationship("Relationship", entityIDSource, entityIDDestination, {});
-  
-  QueryResultsHandler handler(*db);
+  QueryResultsHandler handler(*dbWrapper);
   
   handler.run("MATCH (n) RETURN id(n)");
   
@@ -283,31 +317,36 @@ TEST(Test, SingleNonRecursiveRelationship)
     const auto actual = std::set<std::optional<std::string>>{handler.rows()[0][0], handler.rows()[1][0]};
     EXPECT_EQ(expected, actual);
   }
-  
+  EXPECT_EQ(1, handler.countSQLQueries());
+
   handler.run("MATCH ()-[r]->() RETURN id(r)");
   
   EXPECT_EQ(1, handler.countRows());
   EXPECT_EQ(relationshipID, handler.rows()[0][0]);
-  
+  EXPECT_EQ(1, handler.countSQLQueries());
+
   handler.run("MATCH ()-[r]->() RETURN r.propertyDoesNotExist");
   
   EXPECT_EQ(1, handler.countRows());
   EXPECT_EQ(std::nullopt, handler.rows()[0][0]);
-  
+  EXPECT_EQ(1, handler.countSQLQueries());
+
   handler.run("MATCH (a)-[r]->(b) RETURN id(a), id(r), id(b)");
   
   EXPECT_EQ(1, handler.countRows());
   EXPECT_EQ(relationshipID, handler.rows()[0][1]);
   EXPECT_EQ(entityIDSource, handler.rows()[0][0]);
   EXPECT_EQ(entityIDDestination, handler.rows()[0][2]);
-  
+  EXPECT_EQ(1, handler.countSQLQueries());
+
   handler.run("MATCH (a)<-[r]-(b) RETURN id(a), id(r), id(b)");
   
   EXPECT_EQ(1, handler.countRows());
   EXPECT_EQ(relationshipID, handler.rows()[0][1]);
   EXPECT_EQ(entityIDDestination, handler.rows()[0][0]);
   EXPECT_EQ(entityIDSource, handler.rows()[0][2]);
-  
+  EXPECT_EQ(1, handler.countSQLQueries());
+
   handler.run("MATCH (a)-[R]-(B) RETURN id(a), id(R), id(B)");
   
   EXPECT_EQ(2, handler.countRows());
@@ -321,19 +360,23 @@ TEST(Test, SingleNonRecursiveRelationship)
     const auto actual = std::set<std::optional<std::string>>{handler.rows()[1][0], handler.rows()[1][2]};
     EXPECT_EQ(expected, actual);
   }
-  
+  EXPECT_EQ(2, handler.countSQLQueries());  // TODO optimize
+
   handler.run("MATCH (a)-[r]->(a) RETURN id(a), id(r)");
   
   EXPECT_EQ(0, handler.countRows());
-  
+  EXPECT_EQ(1, handler.countSQLQueries());
+
   handler.run("MATCH (a)<-[r]-(a) RETURN id(a), id(r)");
   
   EXPECT_EQ(0, handler.countRows());
-  
+  EXPECT_EQ(1, handler.countSQLQueries());
+
   handler.run("MATCH (a)-[R]-(a) RETURN id(a), id(R)");
   
   EXPECT_EQ(0, handler.countRows());
-  
+  EXPECT_EQ(2, handler.countSQLQueries());  // TODO optimize
+
   // id(a) <> id(b) is a constraint enforced while scanning the system relationships table.
   handler.run("MATCH (a)-[r]->(b) WHERE id(a) <> id(b) RETURN id(a), id(r), id(b)");
   
@@ -341,10 +384,17 @@ TEST(Test, SingleNonRecursiveRelationship)
   EXPECT_EQ(relationshipID, handler.rows()[0][1]);
   EXPECT_EQ(entityIDSource, handler.rows()[0][0]);
   EXPECT_EQ(entityIDDestination, handler.rows()[0][2]);
-  
+  EXPECT_EQ(1, handler.countSQLQueries());
+
   // id(a) = id(b) is a constraint enforced while scanning the system relationships table.
   handler.run("MATCH (a)-[r]->(b) WHERE id(a) = id(b) RETURN id(a), id(r), id(b)");
   
   EXPECT_EQ(0, handler.countRows());
+  EXPECT_EQ(1, handler.countSQLQueries());
+}
+
+TEST(Test, EntityProperties)
+{
+  
 }
 }
